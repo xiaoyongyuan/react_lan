ad5113e6510994bba9661771907f301c975e14ff
xiaoyongyuan <2207294253@qq.com>
2019-06-26 18:07:44 +0800

    登录

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -8,9 +8,42 @@ class Login extends Component {
 
   }
   render() {
+      const { getFieldDecorator} = this.props.form;
     return (
       <div className="Login">
-        登录
+        <div className="login-title" />
+        <div className="logoBor">
+            <div className="logoBg" />
+            <div className="loginFrame">
+              <p>用户登录</p>
+                <Form className="logoForm">
+                    <Form.Item>
+                        {getFieldDecorator('username', {
+                            rules: [{ required: false, message: 'Please input your username!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.1)' }} />}
+                                placeholder="用户名/邮箱/手机"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item >
+                        {getFieldDecorator('password', {
+                            rules: [{ required: false, message: 'Please input your Password!' }],
+                        })(
+                            <Input
+                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.1)' }} />}
+                                type="password"
+                                placeholder="密码"
+                            />,
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" className="logoBtn">登录</Button>
+                    </Form.Item>
+                </Form>
+            </div>
+        </div>
       </div>
     );
   }
