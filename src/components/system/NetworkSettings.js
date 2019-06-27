504ec017a86ce3bec30fe28e46736010e1614bbe
mikeJang <oncwnuN6uHaFk5pVuYtZaUze_3tk@git.weixin.qq.com>
2019-06-27 09:21:58 +0800

    jhy

--- src/components/system/NetworkSettings.js
+++ src/components/system/NetworkSettings.js
@@ -1,9 +1,95 @@
-import React, { Component } from 'react';
-class NetworkSettings extends Component{
-    render() {
-        return (
-            <div className="userInfo">网络设置</div>
-        );
-    }
+import React, { Component } from "react";
+import { Row, Col, Form, Radio, Button, Input } from "antd";
+import "../../style/jhy/less/netset.less";
+class NetworkSettings extends Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        span: 5
+      },
+      wrapperCol: {
+        span: 16
+      }
+    };
+    return (
+      <div className="netset">
+        <Row>
+          <Row>
+            <Button type="primary">网卡一配置</Button>
+          </Row>
+          <Col span={8}>
+            <Form {...formItemLayout}>
+              <Form.Item label="自动获取IP地址">
+                {getFieldDecorator("autoGetIp", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="使用下面的IP地址">
+                {getFieldDecorator("userUnderIP", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="IP号">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="子网编码">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="默认网关">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="DNS1">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="DNS2">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="网络测试">
+                {getFieldDecorator("userUnderIP", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary">确认</Button>
+                <Button type="default">取消</Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Row>
+            <Button type="primary">网卡二配置</Button>
+          </Row>
+          <Col span={8}>
+            <Form {...formItemLayout}>
+              <Form.Item label="自动获取IP地址">
+                {getFieldDecorator("autoGetIp", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="使用下面的IP地址">
+                {getFieldDecorator("userUnderIP", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="IP号">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="子网编码">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="默认网关">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="DNS1">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="DNS2">
+                {getFieldDecorator("ip", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="网络测试">
+                {getFieldDecorator("userUnderIP", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary">确认</Button>
+                <Button type="default">取消</Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
-export default NetworkSettings;
+export default Form.create({})(NetworkSettings);
--- src/components/system/TimesSettings.js
+++ src/components/system/TimesSettings.js
@@ -1,9 +1,46 @@
-import React, { Component } from 'react';
-class TimesSettings extends Component{
-    render() {
-        return (
-            <div className="userInfo">时间设置</div>
-        );
-    }
+import React, { Component } from "react";
+import { Row, Col, Form, Radio, Button, Input, TimePicker } from "antd";
+import "../../style/jhy/less/timeset.less";
+class TimesSettings extends Component {
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        span: 5
+      },
+      wrapperCol: {
+        span: 16
+      }
+    };
+    return (
+      <div className="timeset">
+        <Row>
+          <Col span={8}>
+            <Form {...formItemLayout}>
+              <Form.Item label="自动时间设置">
+                {getFieldDecorator("autoGettime", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="选择时间获取源">
+                {getFieldDecorator("timeselect", {})(<TimePicker />)}
+              </Form.Item>
+              <Form.Item label="手动时间配置">
+                {getFieldDecorator("handletime", {})(<Radio />)}
+              </Form.Item>
+              <Form.Item label="日期">
+                {getFieldDecorator("date", {})(<Input />)}
+              </Form.Item>
+              <Form.Item label="时间">
+                {getFieldDecorator("time", {})(<Input />)}
+              </Form.Item>
+              <Form.Item>
+                <Button type="primary">确认</Button>
+                <Button type="">取消</Button>
+              </Form.Item>
+            </Form>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
-export default TimesSettings;
+export default Form.create({})(TimesSettings);
