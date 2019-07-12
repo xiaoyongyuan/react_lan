9e9295ff03235325e2287a66805d7a93d9b31b1a
mikeJang <oncwnuN6uHaFk5pVuYtZaUze_3tk@git.weixin.qq.com>
2019-07-12 13:46:51 +0800

    用户编辑合并表单存在bug

--- src/components/system/UserInfo.js
+++ src/components/system/UserInfo.js
@@ -16,19 +16,94 @@ import axiosW from "../../axios/index";
 import moment from "moment";
 import "../../style/jhy/less/userinfo.less";
 const { Option } = Select;
+
+const FormModal = Form.create({ name: "form_in_modal" })(
+  class extends React.Component {
+    componentDidMount() {
+      const { currentRecordData, form } = this.props;
+      form.setFields({
+        account: currentRecordData.account ? currentRecordData.account : "",
+        realname: currentRecordData.realname ? currentRecordData.realname : "",
+        emailaddress: currentRecordData.emailaddress
+          ? currentRecordData.emailaddress
+          : "",
+        utype: currentRecordData.utype ? currentRecordData.utype : 0
+      });
+    }
+
+    render() {
+      const { visible, onCancel, onCreate, form, title } = this.props;
+      const { getFieldDecorator } = form;
+      const formlayout = {
+        labelCol: {
+          span: 5
+        },
+        wrapperCol: {
+          span: 16
+        }
+      };
+      return (
+        <Modal
+          visible={visible}
+          title={`${title}用户`}
+          okText="确定"
+          canCelText="取消"
+          onCancel={onCancel}
+          onOk={onCreate}
+        >
+          <Form {...formlayout}>
+            <Form.Item label="账号" key="account">
+              {getFieldDecorator("account", {
+                rules: [
+                  {
+                    required: true,
+                    message: "请输入账号!"
+                  }
+                ]
+              })(<Input maxLength={10} readOnly={title === "编辑"} />)}
+            </Form.Item>
+            <Form.Item label="用户名" key="realname">
+              {getFieldDecorator("realname", {
+                rules: [
+                  {
+                    required: true,
+                    message: "请输入账号!"
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form.Item label="邮箱地址" key="emailaddress">
+              {getFieldDecorator("emailaddress")(<Input />)}
+            </Form.Item>
+            <Form.Item label="角色权限" key="role">
+              {getFieldDecorator("utype", {
+                initialValue: 0
+              })(
+                <Select>
+                  <Option value={0}>管理员</Option>
+                  <Option value={1}>普通用户</Option>
+                </Select>
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
+
 class UserInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loginUser: "",
       userdata: [],
-      addModel: false
+      visible: false,
+      type: "",
+      title: "",
+      currentRecordData: {}
     };
   }
   componentDidMount() {
-    this.setState({
-      loginUser: localStorage.getItem("account")
-    });
     this.getUserData();
   }
   getUserData = () => {
@@ -49,47 +124,90 @@ class UserInfo extends Component {
         }
       });
   };
-  showAdd = () => {
-    this.setState({
-      addModel: true
-    });
-  };
-  handleAdd = () => {
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        this.setState({
-          addModel: false
-        });
-        axios({
-          method: "post",
-          url: "http://192.168.1.176:8111/api/autocode/auto",
-          data: {
-            account: values.account,
-            emailaddress: values.emailaddress,
-            realname: values.realname,
-            utype: values.utype,
-            ifsys: values.account === "admin" ? 1 : 0,
-            companycode: localStorage.getItem("companycode")
-          }
-        }).then(res => {
-          if (res.data.success) {
-            message.success("添加用户成功");
-            this.getUserData();
-            this.props.form.resetFields();
-          }
-        });
-      } else {
-        message.error(err);
-      }
-    });
+  showModel = opt => {
+    if (opt === "add") {
+      this.setState({
+        visible: true,
+        title: "新增"
+      });
+    } else {
+      this.setState({
+        visible: true,
+        title: "编辑",
+        currentRecordData: opt
+      });
+    }
   };
   handleCancel = () => {
     this.setState({
-      addModel: false
+      visible: false
     });
   };
-  delUser = code => {};
-  editUser = code => {};
+  handleForm = () => {
+    const { form } = this.formRef.props;
+
+    if (this.state.title === "新增") {
+      form.validateFields((err, values) => {
+        if (!err) {
+          this.setState({
+            visible: false
+          });
+          axios({
+            method: "post",
+            url: "http://192.168.1.176:8111/api/autocode/auto",
+            data: {
+              account: values.account,
+              emailaddress: values.emailaddress,
+              realname: values.realname,
+              utype: values.utype,
+              ifsys: values.account === "admin" ? 1 : 0,
+              companycode: localStorage.getItem("companycode")
+            }
+          }).then(res => {
+            if (res.data.success) {
+              message.success("添加用户成功");
+              this.getUserData();
+              form.resetFields();
+            }
+          });
+        } else {
+          message.error(err);
+          return;
+        }
+      });
+    } else {
+      form.validateFields((err, values) => {
+        if (!err) {
+          this.setState({
+            visible: false
+          });
+          axios({
+            method: "post",
+            url: "http://192.168.1.176:8111/api/system/setuser",
+            data: {
+              account: values.account,
+              emailaddress: values.emailaddress,
+              realname: values.realname,
+              utype: values.utype,
+              ifsys: values.account === "admin" ? 1 : 0,
+              companycode: localStorage.getItem("companycode")
+            }
+          }).then(res => {
+            if (res.data.success) {
+              message.success("编辑用户成功");
+              this.getUserData();
+              form.resetFields();
+            }
+          });
+        } else {
+          message.error(err);
+          return;
+        }
+      });
+    }
+  };
+
+  // delUser = code => {};
   render() {
     const ifsys = localStorage.getItem("ifsys");
     const utype = localStorage.getItem("utype");
@@ -98,6 +216,7 @@ class UserInfo extends Component {
       {
         title: "序号",
         align: "center",
+        key: "num",
         render: (text, record, index) => index + 1
       },
       {
@@ -146,36 +265,78 @@ class UserInfo extends Component {
         align: "center",
         width: "18%",
         render: (text, record) => {
-          if (ifsys === 1) {
-            if (account === record.account) {
+          if (ifsys === "1") {
+            if (account == record.account) {
               return null;
             } else {
               return (
                 <span>
-                  <Button type="danger">新增</Button>
+                  <Button
+                    type="primary"
+                    onClick={() => {
+                      this.showModel("add");
+                    }}
+                  >
+                    新增
+                  </Button>
                   <Divider type="vertical" />
-                  <Button type="primary">编辑</Button>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      this.showModel(record);
+                    }}
+                  >
+                    编辑
+                  </Button>
                   <Divider type="vertical" />
                   <Button type="danger">删除</Button>
                 </span>
               );
             }
           } else {
-            if (utype === 0) {
+            if (utype === "0") {
               if (account == record.account) {
                 return (
                   <span>
-                    <Button type="danger">新增</Button>
+                    <Button
+                      type="primary"
+                      onClick={() => {
+                        this.showModel("add");
+                      }}
+                    >
+                      新增
+                    </Button>
                     <Divider type="vertical" />
-                    <Button type="primary">编辑</Button>
+                    <Button
+                      type="dashed"
+                      onClick={() => {
+                        this.showModel(record);
+                      }}
+                    >
+                      编辑
+                    </Button>
                   </span>
                 );
               } else {
                 return (
                   <span>
-                    <Button type="danger">新增</Button>
+                    <Button
+                      type="primary"
+                      onClick={() => {
+                        this.showModel("add");
+                      }}
+                    >
+                      新增
+                    </Button>
                     <Divider type="vertical" />
-                    <Button type="primary">编辑</Button>
+                    <Button
+                      type="dashed"
+                      onClick={() => {
+                        this.showModel(record);
+                      }}
+                    >
+                      编辑
+                    </Button>
                     <Divider type="vertical" />
                     <Button type="danger">删除</Button>
                   </span>
@@ -185,179 +346,21 @@ class UserInfo extends Component {
               return null;
             }
           }
-
-          if (
-            text === 0 &&
-            record.ifsys === 1 &&
-            localStorage.getItem("account" === record.account)
-          ) {
-            return (
-              <span>
-                <Button disabled={record.ifsys} type="danger">
-                  删除
-                </Button>
-                <Divider type="vertical" />
-                <Button
-                  disabled={this.state.loginUser === "admin"}
-                  type="primary"
-                >
-                  编辑
-                </Button>
-              </span>
-            );
-          } else if (text === 1) {
-            return (
-              <span>
-                <Button
-                  // disabled={this.state.loginUser === "admin"}
-                  onClick={() => {
-                    this.delUser(record.code);
-                  }}
-                  type="danger"
-                >
-                  删除
-                </Button>
-                <Divider type="vertical" />
-                <Button
-                  // disabled={this.state.loginUser === "admin"}
-                  onClick={() => this.editUser(record.code)}
-                  type="primary"
-                >
-                  编辑
-                </Button>
-              </span>
-            );
-          } else {
-            return null;
-          }
         }
       }
     ];
-    const formlayout = {
-      labelCol: {
-        span: 5
-      },
-      wrapperCol: {
-        span: 16
-      }
-    };
-    const { getFieldDecorator } = this.props.form;
+
     return (
       <div className="userInfo">
-        <Row style={{ padding: "20px 0" }}>
-          <Button
-            className="addbtn"
-            onClick={() => {
-              this.showAdd();
-            }}
-          >
-            <Icon type="plus" />
-            添加用户
-          </Button>
-        </Row>
+        <FormModal
+          wrappedComponentRef={formRef => (this.formRef = formRef)}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleForm}
+          title={this.state.title}
+          currentRecordData={this.state.currentRecordData}
+        />
         <Etable dataSource={this.state.userdata} columns={userlist} />
-        <Modal
-          title="用户新增"
-          visible={this.state.addModel}
-          onCancel={this.handleCancel}
-          footer={null}
-        >
-          <Form {...formlayout} onSubmit={this.handleAdd}>
-            <Form.Item label="账号" key="account">
-              {getFieldDecorator("account", {
-                rules: [
-                  {
-                    required: true,
-                    message: "请输入账号!"
-                  }
-                ]
-              })(<Input maxLength={10} />)}
-            </Form.Item>
-            <Form.Item label="用户名" key="realname">
-              {getFieldDecorator("realname", {
-                rules: [
-                  {
-                    required: true,
-                    message: "请输入账号!"
-                  }
-                ]
-              })(<Input />)}
-            </Form.Item>
-            <Form.Item label="邮箱地址" key="emailaddress">
-              {getFieldDecorator("emailaddress")(<Input />)}
-            </Form.Item>
-            <Form.Item label="角色权限" key="role">
-              {getFieldDecorator("utype", {
-                initialValue: 1
-              })(
-                <Select>
-                  <Option value={1}>管理员</Option>
-                  <Option value={0}>普通用户</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item
-              label=" "
-              colon={false}
-              wrapperCol={{ span: 4, push: 6 }}
-            >
-              <Button type="primary" htmlType="submit">
-                确定
-              </Button>
-            </Form.Item>
-          </Form>
-        </Modal>
-        <Modal
-          title="用户编辑"
-          visible={this.state.addModel}
-          onCancel={this.handleCancel}
-          footer={null}
-        >
-          <Form {...formlayout} onSubmit={this.handleAdd}>
-            <Form.Item label="账号" key="account">
-              {getFieldDecorator("account", {
-                rules: [
-                  {
-                    required: true,
-                    message: "请输入账号!"
-                  }
-                ]
-              })(<Input maxLength={10} />)}
-            </Form.Item>
-            <Form.Item label="用户名" key="realname">
-              {getFieldDecorator("realname", {
-                rules: [
-                  {
-                    required: true,
-                    message: "请输入账号!"
-                  }
-                ]
-              })(<Input />)}
-            </Form.Item>
-            <Form.Item label="邮箱地址" key="emailaddress">
-              {getFieldDecorator("emailaddress")(<Input />)}
-            </Form.Item>
-            <Form.Item label="角色权限" key="role">
-              {getFieldDecorator("utype", {
-                initialValue: 1
-              })(
-                <Select>
-                  <Option value={1}>管理员</Option>
-                  <Option value={0}>普通用户</Option>
-                </Select>
-              )}
-            </Form.Item>
-            <Form.Item
-              label=" "
-              colon={false}
-              wrapperCol={{ span: 4, push: 6 }}
-            >
-              <Button type="primary" htmlType="submit">
-                确定
-              </Button>
-            </Form.Item>
-          </Form>
-        </Modal>
       </div>
     );
   }
